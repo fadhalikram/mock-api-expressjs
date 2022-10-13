51e3d13303e5983f3517b33ab5c33f0491e90671
ikram <fadhalikram@gmail.com>
2022-10-13 12:28:15 +0700

    Sample Validation

--- api/sample.js
+++ api/sample.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const { body, validationResult } = require('express-validator');
+
 /* Sample LIST */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   const response = {
     data: {
       list: [
@@ -16,11 +18,11 @@ router.get('/', function(req, res, next) {
     }
   }
 
-  res.send(response);
+  return res.status(200).json(response);
 });
 
 /* Sample DETAIL */
-router.get('/:id', function(req, res, next) {
+router.get('/:id', (req, res) => {
   const response = {
     data: {
       text: "Sample Text",
@@ -29,22 +31,60 @@ router.get('/:id', function(req, res, next) {
     }
   }
 
-  res.send(response)
+  return res.status(200).json(response)
 });
 
 /* Sample CREATE */
-router.post('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post(
+  '/', 
+  body('text').not().isEmpty(),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    
+    const response = {
+      data: {
+        id: 1,
+        text: "Sample Text",
+        number: 1,
+        date: "20-10-2022"
+      }
+    }
+
+    return res.status(200).json(response);
 });
 
 /* Sample UPDATE */
-router.put('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.put(
+  '/', 
+  body('text').not().isEmpty(),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    
+    const response = {
+      data: {
+        id: 1,
+        text: "Sample Text",
+        number: 1,
+        date: "20-10-2022"
+      }
+    }
+
+    return res.status(200).json(response);
 });
 
 /* Sample DELETE */
-router.delete('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.delete('/', (req, res) => {
+  const response = {
+    data: null,
+  }
+
+  return res.status(200).json(response);
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+const port = 3000
 const express = require('express')
 const app = express()
-const port = 3000
 
+app.use(express.json())
+
+// Routing
 const sample = require('./api/sample')
 app.use('/sample', sample)
 
 const sample2 = require('./api/sample2')
 app.use('/sample2', sample2)
 
-app.listen(port, () => {
-    console.log(`App Listening on port ${port}`)
-})
+// Port Information
+app.listen(port)
